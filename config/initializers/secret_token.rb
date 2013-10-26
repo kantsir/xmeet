26bdfc6d76e9e01470d86da99cfdae02baab3a32
roka <kantsir.roman@gmail.com>
2013-10-26 22:57:29 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Xmeet::Application.config.secret_key_base = '13b25a090fc4799fa2d8864147b968aac22f7021a3a8490496542fabe88013538735a38fb0c62ad320ea98d40e681316e80cac4a1273fa7e54990ccaab2a54b1'
